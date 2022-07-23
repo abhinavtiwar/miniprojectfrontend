f3b5c51bdfd824d50ff6777a47fef55cde8b38f9
abhinavtiwar <abhinavtiwari17200@gmail.com>
2022-07-23 15:05:35 +0530

    new..

--- src/Components/FloatingDiv.js
+++ src/Components/FloatingDiv.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {FloatingDiv } from "react";
 import './FloatingDiv.css'
 
 
--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import "./Works.css";
-import Upwork from "../../img/Upwork.png";
-import Fiverr from "../../img/fiverr.png";
-import Amazon from "../../img/amazon.png";
-import Shopify from "../../img/Shopify.png";
-import Facebook from "../../img/Facebook.png";
+import Upwork from "../img/Upwork.png";
+import Fiverr from "../img/fiverr.png";
+import Amazon from "../img/amazon.png";
+import Shopify from "../img/Shopify.png";
+import Facebook from "../img/Facebook.png";
 import { themeContext } from "../Context";
 import { motion } from "framer-motion";
 import {Link} from 'react-scroll'
@@ -12,7 +12,7 @@ const Works = () => {
   // context
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-
+ 
   // transition
   return (
     <div className="works" id="works">
